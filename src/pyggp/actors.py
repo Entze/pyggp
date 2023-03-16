ad0ca03aa4a5134e3b0d4bcc6b4533026ec98bc8
Lukas Grassauer <lukas@grassauer.eu>
2023-03-16 16:52:23 +0100

    docs(pyggp.actors): refine documentation and noqa statements

--- src/pyggp/actors.py
+++ src/pyggp/actors.py
@@ -16,13 +16,15 @@ class Actor:
     """Base class for all actors.
 
     Attributes:
-        startclock: The startclock of the actor
-        playclock: The playclock of the actor
+        startclock: Startclock of the actor
+        playclock: Playclock of the actor
         is_human_actor: Whether the actor is a human actor
     """
 
-    def __init__(self, is_human_actor: bool = False) -> None:
-        """Initializer.
+    # Disables FBT001 (Boolean positional arg in function definition). Reason: is_human_actor is a value of an Actor
+    # object, not a flag.
+    def __init__(self, is_human_actor: bool = False) -> None:  # noqa: FBT001
+        """Initializes Actor.
 
         Args:
             is_human_actor: Whether the actor is a human actor
@@ -33,7 +35,11 @@ class Actor:
         self.is_human_actor: bool = is_human_actor
 
     def __repr__(self) -> str:
-        """Representation."""
+        """Gets representation of Actor.
+
+        Returns:
+            Representation of Actor
+        """
         return (
             f"{self.__class__.__name__}(id={hex(id(self))}, "
             f"is_human_actor={self.is_human_actor!r}, "
@@ -51,13 +57,13 @@ class Actor:
         """Sends the start message to the agent.
 
         Args:
-            role: The role of the agent
-            ruleset: The ruleset of the game
-            startclock_config: The configuration of the startclock
-            playclock_config: The configuration of the playclock
+            role: Role of the agent
+            ruleset: Ruleset of the match
+            startclock_config: Configuration of startclock
+            playclock_config: Configuration of playclock
 
         Raises:
-            ActorTimeoutError: The startclock expired
+            ActorTimeoutError: startclock expired
 
         """
         self.startclock = GameClock(startclock_config)
@@ -80,15 +86,15 @@ class Actor:
         """Sends the play message to the agent.
 
         Args:
-            move_nr: The number of the move
-            view: The current state of the game as seen by the agent
+            move_nr: Number of this move
+            view: Current state of the game as seen by the agent
 
         Returns:
-            The agent's returned move
+            Agent's returned move
 
         Raises:
-            ActorNotStartedError: The playclock is None
-            ActorTimeoutError: The playclock expired
+            ActorNotStartedError: playclock is None
+            ActorTimeoutError: playclock expired
         """
         if self.playclock is None:
             raise ActorNotStartedError
@@ -97,7 +103,9 @@ class Actor:
             move = self._send_play(move_nr, view)
         if self.playclock.is_expired:
             raise ActorTimeoutError
-        return move
+        # Disables RET504 (Unnecessary variable assignment before `return` statement). Reason: Check if playclock is
+        # expired is required before returning the move.
+        return move  # noqa: RET504
 
     def _send_play(self, move_nr: int, view: State) -> Move:
         raise NotImplementedError
@@ -105,7 +113,8 @@ class Actor:
     def send_abort(self) -> None:
         """Sends the abort message to the agent.
 
-        This is called when the game is aborted (ended prematurely).
+        Messages the agent that the game ended in an abnormal way. This may be in any state of the game, including
+        terminal states.
 
         """
         self.startclock = None
@@ -118,7 +127,7 @@ class Actor:
     def send_stop(self, view: State) -> None:
         """Sends the stop message to the agent.
 
-        This is called when the game has reached a terminal state.
+        Messages the agent that the game has reached a terminal state.
 
         Args:
             view: The current state of the game as seen by the agent
@@ -135,15 +144,17 @@ class LocalActor(Actor):
     """Actor that communicates with an agent via python method calls.
 
     Attributes:
-        agent: The agent that is controlled by this actor
+        agent: Agent that is controlled by this actor
         is_human_actor: Whether the actor is a human actor
     """
 
-    def __init__(self, agent: Agent, is_human_actor: bool = False) -> None:
-        """Initializer.
+    # Disables FBT001 (Boolean positional arg in function definition). Reason: is_human_actor is a value of an Actor
+    # object, not a flag.
+    def __init__(self, agent: Agent, is_human_actor: bool = False) -> None:  # noqa: FBT001
+        """Initializes LocalActor.
 
         Args:
-            agent: The agent that is controlled by this actor
+            agent: Agent that is controlled by this actor
             is_human_actor: Whether the actor is a human actor
         """
         super().__init__(is_human_actor=is_human_actor)
