533cca3423653eea3f5efffd42c980964e093e08
Lukas Grassauer <lukas@grassauer.eu>
2023-03-23 17:26:03 +0100

    feat(pyggp._logging): make module stable

--- src/pyggp/_logging.py
+++ src/pyggp/_logging.py
@@ -1,4 +1,6 @@
+import datetime
 import logging
+from typing import Final, Union
 
 import inflection
 import rich.logging
@@ -26,13 +28,73 @@ logging.basicConfig(
 log: logging.Logger = logging.getLogger("pyggp")
 
 
-def inflect(noun: str, count: int = 0, with_count: bool = True) -> str:
-    if count == 1:
-        noun_ = inflection.singularize(noun)
-    else:
-        noun_ = inflection.pluralize(noun)
-    if with_count:
-        count_str = f"{count} "
-    else:
-        count_str = ""
-    return f"{count_str}{noun_}"
+def inflect(noun: str, count: int = 0) -> str:
+    """Inflect a noun based on a count.
+
+    Args:
+        noun: Noun to inflect.
+        count: Count to use for inflection.
+
+    Keyword Args:
+        with_count: Whether to include the count in the output.
+
+    Examples:
+        >>> inflect("cat", 0)
+        '0 cats'
+        >>> inflect("cat", 1)
+        '1 cat'
+        >>> inflect("cat", 2)
+        '2 cats'
+
+    """
+    return f"{count} {inflection.pluralize(noun) if count != 1 else inflection.singularize(noun)}"
+
+
+_ONE_MINUTE_IN_S: Final[float] = 60.0
+_ONE_HOUR_IN_S: Final[float] = _ONE_MINUTE_IN_S * 60.0
+_ONE_HUNDRED_MILLISECONDS_IN_S: Final[float] = 0.1
+_ONE_MILLISECOND_IN_S: Final[float] = 0.001
+
+
+def format_timedelta(delta: Union[float, int, datetime.timedelta]) -> str:
+    """Format a timedelta as a human-readable string.
+
+    Args:
+        delta: Delta to format. Float and int are interpreted as seconds.
+
+    Returns:
+        Human-readable string representation of the timedelta.
+
+    Examples:
+        >>> format_timedelta(datetime.timedelta(seconds=0))
+        '0s'
+        >>> format_timedelta(0.1)
+        '0.10s'
+        >>> format_timedelta(0.0001)
+        '0.1000ms'
+        >>> format_timedelta(20)
+        '20.00s'
+        >>> format_timedelta(60)
+        '60.00s'
+        >>> format_timedelta(61)
+        '01:01'
+        >>> format_timedelta(datetime.timedelta(hours=2, minutes=31, seconds=13))
+        '2:31:13'
+
+    """
+    if isinstance(delta, (float, int)):
+        delta = datetime.timedelta(seconds=delta)
+    assert isinstance(delta, datetime.timedelta)
+    total_seconds: float = delta.total_seconds()
+    if total_seconds == 0:
+        return "0s"
+    if total_seconds > _ONE_HOUR_IN_S:
+        return str(delta)
+    if _ONE_MINUTE_IN_S < total_seconds < _ONE_HOUR_IN_S:
+        minutes, seconds = divmod(total_seconds, _ONE_MINUTE_IN_S)
+        return f"{int(minutes):02d}:{int(seconds):02d}"
+    if total_seconds >= _ONE_HUNDRED_MILLISECONDS_IN_S:
+        return f"{total_seconds:.2f}s"
+    if total_seconds >= _ONE_MILLISECOND_IN_S:
+        return f"{total_seconds * 1000:.0f}ms"
+    return f"{total_seconds * 1000:.4f}ms"
---
+++ tests/pyggp/test_protected_logging.py
@@ -0,0 +1,40 @@
+import datetime
+from typing import Union
+
+import pytest
+from pyggp._logging import format_timedelta, inflect
+
+
+@pytest.mark.parametrize(
+    ("noun", "count", "expected"),
+    [
+        ("cat", 0, "0 cats"),
+        ("cat", 1, "1 cat"),
+        ("cat", 2, "2 cats"),
+    ],
+)
+def test_inflect_as_expected(noun: str, count: int, expected: str) -> None:
+    assert inflect(noun, count) == expected
+
+
+@pytest.mark.parametrize(
+    ("delta", "expected"),
+    [
+        (datetime.timedelta(seconds=0), "0s"),
+        (0, "0s"),
+        (0.0, "0s"),
+        (datetime.timedelta(seconds=0.1), "0.10s"),
+        (datetime.timedelta(seconds=20), "20.00s"),
+        (datetime.timedelta(seconds=60), "60.00s"),
+        (datetime.timedelta(seconds=61), "01:01"),
+        (datetime.timedelta(seconds=61, hours=1), str(datetime.timedelta(seconds=61, hours=1))),
+        (datetime.timedelta(seconds=0.99), "0.99s"),
+        (datetime.timedelta(seconds=0.099), "99ms"),
+        (datetime.timedelta(seconds=0.001), "1ms"),
+        (datetime.timedelta(seconds=0.0005), "0.5000ms"),
+        (datetime.timedelta(seconds=0.00001), "0.0100ms"),
+        (datetime.timedelta(seconds=0.000001), "0.0010ms"),
+    ],
+)
+def test_format_timedelta_as_expected(delta: Union[float, int, datetime.timedelta], expected: str) -> None:
+    assert format_timedelta(delta) == expected
