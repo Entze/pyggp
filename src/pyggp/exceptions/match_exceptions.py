73b6b28e149b6a1449d1c9b36d4ed6a87317c872
Lukas Grassauer <lukas@grassauer.eu>
2023-03-24 11:24:35 +0100

    feat(pyggp.exceptions.match_exceptions): make module stable

--- src/pyggp/exceptions/match_exceptions.py
+++ src/pyggp/exceptions/match_exceptions.py
@@ -1,36 +1,102 @@
+"""Exceptions regarding matches."""
+from typing import Optional
+
 from pyggp.actors import Actor
 from pyggp.gdl import Move, Role
 
 
 class MatchError(Exception):
-    pass
+    """Base class for all exceptions regarding matches."""
 
 
-class MatchNotStartedError(MatchError):
-    pass
+class AbortedMatchError(MatchError):
+    """Base class for all exceptions regarding aborted matches."""
 
 
-class MatchPrematurelyTerminatedError(MatchError):
-    pass
+class DidNotStartMatchError(AbortedMatchError):
+    """Match did not start."""
 
+    def __init__(self, /, actor: Optional[Role], role: Optional[Role] = None) -> None:
+        """Initializes DidNotStartMatchError.
 
-class MatchDNSError(MatchPrematurelyTerminatedError):
-    def __init__(self, role: Role, actor: Actor):
-        message = f"{role=}, {actor=}"
+        Args:
+            actor: Actor that caused the match to not start
+            role: Role inhabited by actor that caused the match to not start
+
+        """
+        role_message = f" caused by role {role}" if role is not None else ""
+        actor_message = f" caused by actor {actor}" if actor is not None else ""
+        message = f"Match did not start{actor_message}{role_message}"
         super().__init__(message)
 
 
-class MatchDNFError(MatchPrematurelyTerminatedError):
-    def __init__(self, move_nr: int, actor: Actor, role: Role, reason: str) -> None:
-        message = f"Match terminated prematurely after move {move_nr} by {actor} as {role}: {reason}"
+class DidNotFinishMatchError(AbortedMatchError):
+    """Match did not finish."""
+
+    def __init__(
+        self,
+        /,
+        actor: Optional[Actor] = None,
+        ply: Optional[int] = None,
+        reason: Optional[str] = None,
+        role: Optional[Role] = None,
+    ) -> None:
+        """Initializes DidNotFinishMatchError.
+
+        Args:
+            actor: Actor that caused the match to not finish
+            ply: Current ply
+            reason: Reason why the match did not finish
+            role: Role inhabited by actor that caused the match to not finish
+
+        """
+        ply_message = f" during ply {ply}" if ply is not None else ""
+        actor_message = f" caused by actor {actor}" if actor is not None else ""
+        reason_message = f" because {reason}" if reason is not None else ""
+        role_message = f" caused by role {role}" if role is not None else ""
+        message = f"Match did not finish{reason_message}{ply_message}{actor_message}{role_message}"
         super().__init__(message)
 
 
-class MatchTimeoutError(MatchDNFError):
-    def __init__(self, move_nr: int, actor: Actor, role: Role) -> None:
-        super().__init__(move_nr, actor, role, "timeout")
+class TimeoutMatchError(DidNotFinishMatchError):
+    """Match timed out."""
+
+    def __init__(
+        self,
+        /,
+        actor: Optional[Actor] = None,
+        ply: Optional[int] = None,
+        role: Optional[Role] = None,
+    ) -> None:
+        """Initializes MatchTimeoutError.
+
+        Args:
+            actor: Actor that caused the match to time out
+            ply: Current ply
+            role: Role inhabited by actor that caused the match to time out
+
+        """
+        super().__init__(actor=actor, ply=ply, reason="timeout", role=role)
 
 
-class MatchIllegalMoveError(MatchDNFError):
-    def __init__(self, move_nr: int, actor: Actor, role: Role, move: Move) -> None:
-        super().__init__(move_nr, actor, role, f"illegal move({move})")
+class IllegalMoveMatchError(DidNotFinishMatchError):
+    """Illegal move attempted to be played."""
+
+    def __init__(
+        self,
+        /,
+        actor: Optional[Actor] = None,
+        move: Optional[Move] = None,
+        ply: Optional[int] = None,
+        role: Optional[Role] = None,
+    ) -> None:
+        """Initializes IllegalMoveMatchError.
+
+        Args:
+            actor: Actor that attempted to play the illegal move
+            move: Illegal move
+            ply: Current ply
+            role: Role inhabited by actor that attempted to play the illegal move
+
+        """
+        super().__init__(actor=actor, ply=ply, reason=f"illegal move({move})", role=role)
