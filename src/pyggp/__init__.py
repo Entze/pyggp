3f47884aca1e50eed8d59159e1e2592ec65f6e53
Lukas Grassauer <lukas@grassauer.eu>
2023-03-24 11:50:10 +0100

    docs(pyggp): add reference to documentation for more information

--- src/pyggp/__init__.py
+++ src/pyggp/__init__.py
@@ -1,6 +1,6 @@
 """A modernized python implementation of the Stanford GGP project.
 
-TODO: Longer description.
+See the documentation for detailed information.
 
 """
 from ._version import __version__
