9eb1f97710e454e39f73af7425178914843e899f
Lukas Grassauer <lukas@grassauer.eu>
2023-04-27 17:41:10 +0200

    feat(pyggp.agents): refactor agents to be dataclasses

--- src/pyggp/exceptions/agent_exceptions.py
+++ src/pyggp/exceptions/agent_exceptions.py
@@ -5,13 +5,17 @@ class AgentError(Exception):
     """Base class for all exceptions regarding agents."""
 
 
+class RoleIsNoneAgentError(AgentError):
+    """Accessed role when it was None."""
+
+
+class PlayclockConfigurationIsNoneAgentError(AgentError):
+    """Accessed playclock_configuration when it was None."""
+
+
 class InterpreterAgentError(AgentError):
     """Base class for all exceptions regarding interpreter agents."""
 
 
 class InterpreterIsNoneInterpreterAgentError(InterpreterAgentError):
     """Accessed interpreter when it was None."""
-
-
-class RoleIsNoneInterpreterAgentError(InterpreterAgentError):
-    """Accessed role when it was None."""
