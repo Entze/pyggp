b7f341f102900bad84f6a6312fdd825198500fca
Lukas Grassauer <lukas@grassauer.eu>
2023-06-19 22:36:33 +0200

    perf: add phantom connect mcts benchmark

---
+++ prof/profile_pc564_step_mcts_agent.py
@@ -0,0 +1,37 @@
+from pyggp.agents import MCTSAgent, SOISMCTSAgent
+from pyggp.gameclocks import DEFAULT_START_CLOCK_CONFIGURATION, GameClock
+from tqdm import trange
+
+from prof.prof_caches import clear_caches, print_cache_info
+from prof.prof_common_phantom_connect_5_6_4 import (
+    phantom_connect_5_6_4_init_state,
+    phantom_connect_5_6_4_interpreter,
+    phantom_connect_5_6_4_role_x,
+    phantom_connect_5_6_4_ruleset,
+)
+
+agent = SOISMCTSAgent(interpreter=phantom_connect_5_6_4_interpreter)
+
+with agent:
+    agent.prepare_match(
+        role=phantom_connect_5_6_4_role_x,
+        ruleset=phantom_connect_5_6_4_ruleset,
+        startclock_config=GameClock.Configuration(total_time=0.0, delay=1.0),
+        playclock_config=GameClock.Configuration(total_time=2.5, delay=2.5),
+    )
+
+    init_view = agent.interpreter.get_sees_by_role(phantom_connect_5_6_4_init_state, agent.role)
+
+    print("Calculating move...")
+    agent.calculate_move(ply=0, total_time_ns=0, view=init_view)
+
+    print()
+    print_cache_info(agent.interpreter)
+    clear_caches(agent.interpreter)
+
+    for _ in trange(250):
+        agent.step()
+
+    print()
+    print_cache_info(agent.interpreter)
+print()
