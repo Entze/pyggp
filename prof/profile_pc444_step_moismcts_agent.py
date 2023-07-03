000f1736bbae6537bf4c2f09880c18466cb11d12
Lukas Grassauer <lukas@grassauer.eu>
2023-07-03 15:21:56 +0200

    perf: add profiling code for phantom_connect(4,4,4)

---
+++ prof/profile_pc444_step_moismcts_agent.py
@@ -0,0 +1,37 @@
+from pyggp.agents import MOISMCTSAgent
+from pyggp.gameclocks import GameClock
+from tqdm import trange
+
+from prof.prof_caches import clear_caches, print_cache_info
+from prof.prof_common_phantom_connect_4_4_4 import (
+    phantom_connect_4_4_4_init_state,
+    phantom_connect_4_4_4_interpreter,
+    phantom_connect_4_4_4_role_x,
+    phantom_connect_4_4_4_ruleset,
+)
+
+agent = MOISMCTSAgent(interpreter=phantom_connect_4_4_4_interpreter)
+
+with agent:
+    agent.prepare_match(
+        role=phantom_connect_4_4_4_role_x,
+        ruleset=phantom_connect_4_4_4_ruleset,
+        startclock_config=GameClock.Configuration(total_time=0.0, delay=1.0),
+        playclock_config=GameClock.Configuration(total_time=2.5, delay=2.5),
+    )
+
+    init_view = agent.interpreter.get_sees_by_role(phantom_connect_4_4_4_init_state, agent.role)
+
+    print("Calculating move...")
+    agent.calculate_move(ply=0, total_time_ns=0, view=init_view)
+
+    print()
+    print_cache_info(agent.interpreter)
+    clear_caches(agent.interpreter)
+
+    for _ in trange(10_000):
+        agent.step()
+
+    print()
+    print_cache_info(agent.interpreter)
+print()
