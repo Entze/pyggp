c292642bb93f0a55af6ce1901e9eb89a7a7fa18b
Lukas Grassauer <lukas@grassauer.eu>
2023-07-06 09:09:39 +0200

    feat: add kalaha(4,3)

--- tests/games/test_kalaha.py
+++ tests/games/test_kalaha.py
@@ -3,13 +3,14 @@ from typing import Mapping, Optional
 
 import pyggp.game_description_language as gdl
 import pytest
-from pyggp.engine_primitives import Move, Role, State, Turn
+from pyggp.engine_primitives import Development, DevelopmentStep, Move, Role, State, Turn
 from pyggp.interpreters import ClingoInterpreter, Interpreter
+from pyggp.records import PerfectInformationRecord
 
 
 @pytest.fixture(scope="session")
 def kalaha_str() -> str:
-    return pathlib.Path("../src/games/kalaha(6,6).gdl").read_text()
+    return pathlib.Path("../src/games/kalaha(4,3).gdl").read_text()
 
 
 @pytest.fixture(scope="session")
@@ -52,6 +53,8 @@ def get_state(
     role_in_control: Optional[Role] = None,
     stores: Optional[Mapping[Role, int]] = None,
     houses: Optional[Mapping[Role, Mapping[int, int]]] = None,
+    nr_of_houses: int = 4,
+    nr_of_stones: int = 0,
 ) -> State:
     subrelations = set()
     roles = frozenset((Role(gdl.Subrelation(gdl.Relation("south"))), Role(gdl.Subrelation(gdl.Relation("north")))))
@@ -60,8 +63,8 @@ def get_state(
     houses = houses or {}
     for role in roles:
         subrelations.add(store(role, stores.get(role, 0)))
-        for house_ in range(1, 7):
-            subrelations.add(house(role, house_, houses.get(role, {}).get(house_, 0)))
+        for house_ in range(1, nr_of_houses + 1):
+            subrelations.add(house(role, house_, houses.get(role, {}).get(house_, nr_of_stones)))
 
     if role_in_control is not None:
         subrelations.add(control(role_in_control))
@@ -82,7 +85,8 @@ def test_init_state(kalaha_interpreter, north, south) -> None:
 
     expected = get_state(
         north,
-        houses={north: {1: 6, 2: 6, 3: 6, 4: 6, 5: 6, 6: 6}, south: {1: 6, 2: 6, 3: 6, 4: 6, 5: 6, 6: 6}},
+        nr_of_houses=4,
+        nr_of_stones=3,
     )
 
     assert actual == expected
@@ -92,7 +96,7 @@ def test_init_state(kalaha_interpreter, north, south) -> None:
 def test_next_state_single_stone_non_capture(kalaha_interpreter, p1, p2, request) -> None:
     p1 = request.getfixturevalue(p1)
     p2 = request.getfixturevalue(p2)
-    state = get_state(p1, houses={p1: {1: 1, 2: 1}, p2: {6: 1}})
+    state = get_state(p1, houses={p1: {1: 1, 2: 1}, p2: {4: 1}})
 
     move = Move(gdl.Subrelation(gdl.Number(1)))
 
@@ -100,7 +104,7 @@ def test_next_state_single_stone_non_capture(kalaha_interpreter, p1, p2, request
 
     actual = kalaha_interpreter.get_next_state(state, turn)
 
-    expected = get_state(p2, houses={p1: {2: 2}, p2: {6: 1}})
+    expected = get_state(p2, houses={p1: {2: 2}, p2: {4: 1}})
 
     assert actual == expected
 
@@ -126,7 +130,7 @@ def test_next_state_single_stone_capture_empty(kalaha_interpreter, p1, p2, reque
 def test_next_state_single_stone_capture_nonempty(kalaha_interpreter, p1, p2, request) -> None:
     p1 = request.getfixturevalue(p1)
     p2 = request.getfixturevalue(p2)
-    state = get_state(p1, houses={p1: {1: 1}, p2: {5: 1, 6: 1}})
+    state = get_state(p1, houses={p1: {1: 1}, p2: {3: 1, 4: 1}})
 
     move = Move(gdl.Subrelation(gdl.Number(1)))
 
@@ -134,7 +138,7 @@ def test_next_state_single_stone_capture_nonempty(kalaha_interpreter, p1, p2, re
 
     actual = kalaha_interpreter.get_next_state(state, turn)
 
-    expected = get_state(p2, stores={p1: 2}, houses={p2: {6: 1}})
+    expected = get_state(p2, stores={p1: 2}, houses={p2: {4: 1}})
 
     assert actual == expected
 
@@ -143,9 +147,9 @@ def test_next_state_single_stone_capture_nonempty(kalaha_interpreter, p1, p2, re
 def test_next_state_single_stone_move_again(kalaha_interpreter, p1, p2, request) -> None:
     p1 = request.getfixturevalue(p1)
     p2 = request.getfixturevalue(p2)
-    state = get_state(p1, houses={p1: {6: 1, 1: 1}})
+    state = get_state(p1, houses={p1: {4: 1, 1: 1}})
 
-    move = Move(gdl.Subrelation(gdl.Number(6)))
+    move = Move(gdl.Subrelation(gdl.Number(4)))
 
     turn = Turn({p1: move})
 
@@ -161,7 +165,7 @@ def test_next_state_move_again(kalaha_interpreter, p1, p2, request) -> None:
     p1 = request.getfixturevalue(p1)
     p2 = request.getfixturevalue(p2)
 
-    state = get_state(p1, houses={p1: {1: 6}})
+    state = get_state(p1, houses={p1: {1: 4}})
 
     move = Move(gdl.Subrelation(gdl.Number(1)))
 
@@ -169,7 +173,7 @@ def test_next_state_move_again(kalaha_interpreter, p1, p2, request) -> None:
 
     actual = kalaha_interpreter.get_next_state(state, turn)
 
-    expected = get_state(p1, stores={p1: 1}, houses={p1: {1: 0, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1}})
+    expected = get_state(p1, stores={p1: 1}, houses={p1: {1: 0, 2: 1, 3: 1, 4: 1}})
 
     assert actual == expected
 
@@ -179,15 +183,15 @@ def test_next_state_capture(kalaha_interpreter, p1, p2, request) -> None:
     p1 = request.getfixturevalue(p1)
     p2 = request.getfixturevalue(p2)
 
-    state = get_state(p1, houses={p1: {3: 2}, p2: {2: 3, 1: 1}})
+    state = get_state(p1, houses={p1: {1: 2}, p2: {2: 3, 1: 1}})
 
-    move = Move(gdl.Subrelation(gdl.Number(3)))
+    move = Move(gdl.Subrelation(gdl.Number(1)))
 
     turn = Turn({p1: move})
 
     actual = kalaha_interpreter.get_next_state(state, turn)
 
-    expected = get_state(p2, stores={p1: 4}, houses={p1: {4: 1}, p2: {1: 1}})
+    expected = get_state(p2, stores={p1: 4}, houses={p1: {2: 1}, p2: {1: 1}})
 
     assert actual == expected
 
@@ -197,9 +201,9 @@ def test_next_state_spill(kalaha_interpreter, p1, p2, request) -> None:
     p1 = request.getfixturevalue(p1)
     p2 = request.getfixturevalue(p2)
 
-    state = get_state(p1, houses={p1: {6: 2}})
+    state = get_state(p1, houses={p1: {4: 2}})
 
-    move = Move(gdl.Subrelation(gdl.Number(6)))
+    move = Move(gdl.Subrelation(gdl.Number(4)))
 
     turn = Turn({p1: move})
 
@@ -215,15 +219,15 @@ def test_next_state_skips_other_store(kalaha_interpreter, p1, p2, request) -> No
     p1 = request.getfixturevalue(p1)
     p2 = request.getfixturevalue(p2)
 
-    state = get_state(p1, houses={p1: {6: 8, 1: 1}})
+    state = get_state(p1, houses={p1: {4: 6, 1: 1}})
 
-    move = Move(gdl.Subrelation(gdl.Number(6)))
+    move = Move(gdl.Subrelation(gdl.Number(4)))
 
     turn = Turn({p1: move})
 
     actual = kalaha_interpreter.get_next_state(state, turn)
 
-    expected = get_state(p2, stores={p1: 1}, houses={p1: {1: 2}, p2: {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1}})
+    expected = get_state(p2, stores={p1: 1}, houses={p1: {1: 2}, p2: {1: 1, 2: 1, 3: 1, 4: 1}})
 
     assert actual == expected
 
@@ -233,15 +237,15 @@ def test_next_state_spill_capture(kalaha_interpreter, p1, p2, request) -> None:
     p1 = request.getfixturevalue(p1)
     p2 = request.getfixturevalue(p2)
 
-    state = get_state(p1, houses={p1: {6: 8}, p2: {6: 2}})
+    state = get_state(p1, houses={p1: {4: 6}, p2: {4: 2}})
 
-    move = Move(gdl.Subrelation(gdl.Number(6)))
+    move = Move(gdl.Subrelation(gdl.Number(4)))
 
     turn = Turn({p1: move})
 
     actual = kalaha_interpreter.get_next_state(state, turn)
 
-    expected = get_state(p2, stores={p1: 5}, houses={p2: {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}})
+    expected = get_state(p2, stores={p1: 5}, houses={p2: {1: 1, 2: 1, 3: 1}})
 
     assert actual == expected
 
@@ -251,7 +255,7 @@ def test_next_state_overspill(kalaha_interpreter, p1, p2, request) -> None:
     p1 = request.getfixturevalue(p1)
     p2 = request.getfixturevalue(p2)
 
-    state = get_state(p1, houses={p1: {3: 14}})
+    state = get_state(p1, houses={p1: {3: 10}})
 
     move = Move(gdl.Subrelation(gdl.Number(3)))
 
@@ -261,8 +265,8 @@ def test_next_state_overspill(kalaha_interpreter, p1, p2, request) -> None:
 
     expected = get_state(
         p2,
-        stores={p1: 1},  # TODO: p2 is 1 but should be 0
-        houses={p1: {4: 2, 5: 1, 6: 1, 1: 1, 2: 1, 3: 1}, p2: {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1}},
+        stores={p1: 1},
+        houses={p1: {1: 1, 2: 1, 3: 1, 4: 2}, p2: {1: 1, 2: 1, 3: 1, 4: 1}},
     )
 
     assert actual == expected
@@ -273,9 +277,9 @@ def test_next_state_overspill_move_again(kalaha_interpreter, p1, p2, request) ->
     p1 = request.getfixturevalue(p1)
     p2 = request.getfixturevalue(p2)
 
-    state = get_state(p1, houses={p1: {6: 14}})
+    state = get_state(p1, houses={p1: {4: 10}})
 
-    move = Move(gdl.Subrelation(gdl.Number(6)))
+    move = Move(gdl.Subrelation(gdl.Number(4)))
 
     turn = Turn({p1: move})
 
@@ -284,7 +288,7 @@ def test_next_state_overspill_move_again(kalaha_interpreter, p1, p2, request) ->
     expected = get_state(
         p1,
         stores={p1: 2},
-        houses={p1: {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1}, p2: {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1}},
+        houses={p1: {1: 1, 2: 1, 3: 1, 4: 1}, p2: {1: 1, 2: 1, 3: 1, 4: 1}},
     )
 
     assert actual == expected
@@ -295,7 +299,7 @@ def test_next_state_overspill_capture(kalaha_interpreter, p1, p2, request) -> No
     p1 = request.getfixturevalue(p1)
     p2 = request.getfixturevalue(p2)
 
-    state = get_state(p1, houses={p1: {3: 13}, p2: {4: 1}})
+    state = get_state(p1, houses={p1: {3: 9}, p2: {2: 1}})
 
     move = Move(gdl.Subrelation(gdl.Number(3)))
 
@@ -306,44 +310,8 @@ def test_next_state_overspill_capture(kalaha_interpreter, p1, p2, request) -> No
     expected = get_state(
         p2,
         stores={p1: 4},
-        houses={p1: {4: 1, 5: 1, 6: 1, 3: 0, 1: 1, 2: 1}, p2: {1: 1, 2: 1, 3: 1, 4: 0, 5: 1, 6: 1}},
+        houses={p1: {4: 1, 3: 0, 1: 1, 2: 1}, p2: {1: 1, 2: 0, 3: 1, 4: 1}},
     )
-
-    assert actual == expected
-
-
-@pytest.mark.parametrize(("p1", "p2"), [("north", "south"), ("south", "north")])
-def test_next_state_sweep_on_move_again(kalaha_interpreter, p1, p2, request) -> None:
-    p1 = request.getfixturevalue(p1)
-    p2 = request.getfixturevalue(p2)
-
-    state = get_state(p1, houses={p1: {6: 1}, p2: {5: 5}})
-
-    move = Move(gdl.Subrelation(gdl.Number(6)))
-
-    turn = Turn({p1: move})
-
-    actual = kalaha_interpreter.get_next_state(state, turn)
-
-    expected = get_state(stores={p1: 1, p2: 5})
-
-    assert actual == expected
-
-
-@pytest.mark.parametrize(("p1", "p2"), [("north", "south"), ("south", "north")])
-def test_next_state_sweep(kalaha_interpreter, p1, p2, request) -> None:
-    p1 = request.getfixturevalue(p1)
-    p2 = request.getfixturevalue(p2)
-
-    state = get_state(p1, houses={p1: {4: 2, 6: 1, 1: 5}})
-
-    move = Move(gdl.Subrelation(gdl.Number(4)))
-
-    turn = Turn({p1: move})
-
-    actual = kalaha_interpreter.get_next_state(state, turn)
-
-    expected = get_state(stores={p1: 8, p2: 0})
 
     assert actual == expected
 
@@ -353,11 +321,11 @@ def test_legal_all(kalaha_interpreter, p1, p2, request) -> None:
     p1 = request.getfixturevalue(p1)
     p2 = request.getfixturevalue(p2)
 
-    state = get_state(p1, houses={p1: {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1}})
+    state = get_state(p1, houses={p1: {1: 1, 2: 1, 3: 1, 4: 1}})
 
     actual = kalaha_interpreter.get_legal_moves_by_role(state, p1)
 
-    expected = frozenset(Move(gdl.Subrelation(gdl.Number(m))) for m in range(1, 7))
+    expected = frozenset(Move(gdl.Subrelation(gdl.Number(m))) for m in range(1, 5))
 
     assert actual == expected
 
@@ -367,12 +335,12 @@ def test_legal_missing(kalaha_interpreter, p1, p2, request) -> None:
     p1 = request.getfixturevalue(p1)
     p2 = request.getfixturevalue(p2)
 
-    state = get_state(p1, houses={p1: {1: 1, 2: 1, 3: 0, 4: 1, 5: 1, 6: 1}})
+    state = get_state(p1, houses={p1: {1: 1, 2: 1, 3: 0, 4: 1}})
 
     actual = kalaha_interpreter.get_legal_moves_by_role(state, p1)
 
     expected = frozenset(
-        (Move(gdl.Subrelation(gdl.Number(m))) for m in (1, 2, 4, 5, 6)),
+        (Move(gdl.Subrelation(gdl.Number(m))) for m in (1, 2, 4)),
     )
 
     assert actual == expected
@@ -385,3 +353,29 @@ def test_is_terminal(kalaha_interpreter) -> None:
     expected = False
 
     assert actual == expected
+
+
+def test_developments(kalaha_interpreter, north) -> None:
+    state = kalaha_interpreter.get_init_state()
+    move = Move(gdl.Subrelation(gdl.Number(3)))
+    turn = Turn({north: move})
+    next_state = kalaha_interpreter.get_next_state(state, turn)
+
+    record = PerfectInformationRecord(states={0: state, 1: next_state})
+
+    actual = kalaha_interpreter.get_developments(record)
+
+    expected = Development(
+        (
+            DevelopmentStep(
+                state=state,
+                turn=turn,
+            ),
+            DevelopmentStep(
+                state=next_state,
+                turn=None,
+            ),
+        )
+    )
+
+    assert tuple(actual) == expected
