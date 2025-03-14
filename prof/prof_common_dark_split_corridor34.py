d77e24bf47ee5286e15b618b29d9d6a028e391d8
Lukas Grassauer <lukas@grassauer.eu>
2025-03-14 17:46:11 +0100

    Add profile of dark-split-corridor for right in ambiguous state

--- prof/prof_common_dark_split_corridor34.py
+++ prof/prof_common_dark_split_corridor34.py
@@ -1,7 +1,7 @@
 import pathlib
 
 import pyggp.game_description_language as gdl
-from pyggp.engine_primitives import Role
+from pyggp.engine_primitives import Move, Role, Turn
 from pyggp.interpreters import ClingoInterpreter
 
 corridor_str: str = pathlib.Path("../src/games/dark_split_corridor(3,4).gdl").read_text()
@@ -16,3 +16,16 @@ corridor_roles = corridor_interpreter.get_roles()
 
 corridor_left = Role(gdl.Subrelation(gdl.Relation("left")))
 corridor_right = Role(gdl.Subrelation(gdl.Relation("right")))
+
+b2: gdl.Subrelation = gdl.Subrelation(
+    gdl.Relation(None, (gdl.Subrelation(gdl.Relation("b")), gdl.Subrelation(gdl.Number(2))))
+)
+b3: gdl.Subrelation = gdl.Subrelation(
+    gdl.Relation(None, (gdl.Subrelation(gdl.Relation("b")), gdl.Subrelation(gdl.Number(3))))
+)
+b2_b3 = gdl.Subrelation(gdl.Relation(None, (b2, b3)))
+block_b2_b3 = gdl.Subrelation(gdl.Relation("block", (b2_b3,)))
+
+corridor_second_state = corridor_interpreter.get_next_state(
+    corridor_init_state, Turn({corridor_left: Move(block_b2_b3)})
+)
---
+++ prof/profile_dsc_34_snd_step_mcts.py
@@ -0,0 +1,30 @@
+import tqdm
+
+from prof.prof_common_dark_split_corridor34 import (
+    corridor_interpreter,
+    corridor_left,
+    corridor_right,
+    corridor_second_state,
+)
+from pyggp.agents import MOISMCTSAgent
+from pyggp.engine_primitives import View
+from pyggp.gameclocks import GameClock
+
+agent = MOISMCTSAgent(interpreter=corridor_interpreter)
+
+with agent:
+    agent.prepare_match(
+        ruleset=corridor_interpreter.ruleset,
+        role=corridor_right,
+        startclock_config=GameClock.Configuration(total_time=2.5, delay=2.5),
+        playclock_config=GameClock.Configuration(total_time=2.5, delay=2.5),
+    )
+
+    agent.update(1, corridor_interpreter.get_sees_by_role(corridor_second_state, corridor_right), 100_000_000)
+
+    for _ in tqdm.trange(500):
+        agent.step()
+
+    # tree = agent.trees[agent.role]
+    # print(tree.valuation)
+    # print("\n".join(f"{turn} -> {child.valuation}" for turn, child in tree.children.items()))
